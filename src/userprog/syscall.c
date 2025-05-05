2fe00fab1d30ce8934b5759f4da74ddb14e79c02
joe <engjo1010@gmail.com>
2025-05-05 23:22:05 +0300

    Implement syscall handler and validation functions in syscall.c

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -4,17 +4,107 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 
-static void syscall_handler (struct intr_frame *);
+static void syscall_handler(struct intr_frame *f UNUSED);
+static void get_args(struct intr_frame *f, int *args, int num);
 
-void
-syscall_init (void) 
-{
-  intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+/* Checks if the user address is valid */
+bool valid (void * vaddr);
+/* Calls exit with -1 status */
+void kill (void);
+
+
+void syscall_init(void) {
+  intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
-static void
-syscall_handler (struct intr_frame *f UNUSED) 
+
+static void get_args(struct intr_frame *f, int *args, int num)
 {
-  printf ("system call!\n");
-  thread_exit ();
+    for (int i = 0; i < num; i++)
+    {
+        void *ptr = (int *)f->esp + i + 1;
+        check_user_pointer(ptr);
+        args[i] = *(int *)ptr;
+    }
+}
+
+static void syscall_handler(struct intr_frame *f UNUSED) {
+  if(!valid(f->esp)) {
+    kill();
+  }
+  
+  int syscall_number = *(int *)f->esp;
+  int args[3]; 
+
+  switch (syscall_number) {
+    
+    case SYS_HALT:
+      break;
+
+    case SYS_EXIT:
+      break;
+
+    case SYS_EXEC:
+      break;
+
+    case SYS_WAIT:
+      break;
+
+    case SYS_CREATE:
+      break;
+
+    case SYS_REMOVE:
+      break;
+    
+    case SYS_OPEN:
+      break;
+
+    case SYS_FILESIZE:
+      break;
+    
+    case SYS_READ:
+      break;
+    
+    case SYS_WRITE:
+      break;
+
+    case SYS_SEEK:
+      break;
+      
+    case SYS_TELL:
+      break;
+
+    case SYS_CLOSE:
+      break;
+
+    
+    default:
+      printf("Unknown system call number: %d\n", syscall_number);
+      thread_exit();
+  }
+}
+
+
+// SYS_HALT,                   /* Halt the operating system. */
+//     SYS_EXIT,                   /* Terminate this process. */
+//     SYS_EXEC,                   /* Start another process. */
+//     SYS_WAIT,                   /* Wait for a child process to die. */
+//     SYS_CREATE,                 /* Create a file. */
+//     SYS_REMOVE,                 /* Delete a file. */
+//     SYS_OPEN,                   /* Open a file. */
+//     SYS_FILESIZE,               /* Obtain a file's size. */
+//     SYS_READ,                   /* Read from a file. */
+//     SYS_WRITE,                  /* Write to a file. */
+//     SYS_SEEK,                   /* Change position in a file. */
+//     SYS_TELL,                   /* Report current position in a file. */
+//     SYS_CLOSE,                  /* Close a file. */
+
+bool valid(void * vaddr)
+{
+  return (is_user_vaddr(vaddr) && 
+    pagedir_get_page(thread_current()->pagedir,vaddr)!=NULL);
+}
+void kill(void)
+{
+  exit(-1);
 }
